afe431e14d0e380fbae659ff06649725c62c6963
AllegriM <elmonky99@gmail.com>
2022-10-31 20:21:49 -0300

    feat: :rocket: Initialize project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
+import { Banner } from './components/Banner';
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={'./alkemy_logo.svg'} className="App-logo" alt="logo" />
-        <p>
-          Bienvenido a AlkyBank
+        <p className='text-black'>
+          Alkemy
+          <Banner />
         </p>
       </header>
     </div>
