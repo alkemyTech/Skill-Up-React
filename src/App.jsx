ac90ba41854b65f1480037ee52bf34238784ff81
Philip Perez <jperez.c92@gmail.com>
2022-11-01 13:11:54 -0500

    styles: add theme colors and fonts

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header bg-ct-primary-400">
+        <img src={"./alkemy_logo.svg"} className="App-logo" alt="logo" />
+        <p>Bienvenido a AlkyBank</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
