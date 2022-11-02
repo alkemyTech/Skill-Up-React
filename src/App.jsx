da5aff701e31fc4fbad7a42b57bb97a5dbd0612e
Lucas Sansberro <lucas_sansberro@outlook.com>
2022-11-02 10:58:56 -0300

    setStatus added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./App.css";
 import { Heading } from "./components/Heading";
 
+import Tooltip from "./components/Heading/Tooltip/Tooltip";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header bg-ct-primary-400">
-        <img src={"./alkemy_logo.svg"} className="App-logo" alt="logo" />
+        <Tooltip content="Test" status="success">
+          <img src={"./alkemy_logo.svg"} className="App-logo" alt="logo" />
+        </Tooltip>
         <Heading as="h2">Bienvenido a AlkyBank</Heading>
       </header>
     </div>
