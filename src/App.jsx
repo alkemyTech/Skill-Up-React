687c9d30cf7b85b81734e20dff7fc3f445c27e1e
Lucas Sansberro <lucas_sansberro@outlook.com>
2022-11-02 11:07:27 -0300

    Example deleted

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,11 @@ import React from "react";
 import "./App.css";
 import { Heading } from "./components/Heading";
 
-import Tooltip from "./components/Heading/Tooltip/Tooltip";
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header bg-ct-primary-400">
-        <Tooltip content="Test" status="success">
+      <header className="App-header bg-ct-primary-400">    
           <img src={"./alkemy_logo.svg"} className="App-logo" alt="logo" />
-        </Tooltip>
         <Heading as="h2">Bienvenido a AlkyBank</Heading>
       </header>
     </div>
