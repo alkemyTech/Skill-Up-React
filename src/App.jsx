0a66e3294b1ad4bccc96c2bcddb00623949aa54c
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-11-02 20:03:21 -0300

    Rutas de las secciones

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Switch } from 'react-router-dom';
 import './App.css';
+import Balance from './pages/Balance';
+import CargaSaldo from './pages/CargaSaldo';
+import EnvioDinero from './pages/EnvioDinero';
+import Gastos from './pages/Gastos';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Movimiento from './pages/Movimiento';
+import NotFound from './pages/NotFound';
 
 function App() {
 
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<Login />}/>
-        <Route path="/home" element={<Home />} />
+        <Switch>
+          <Route exact path='/' render={() => <Login/>}/>
+          <Route path="/home" element={<Home />} />
+          <Route path="/carga-saldo" element={<CargaSaldo />} />
+          <Route path="/gastos" element={<Gastos />} />
+          <Route path="/balance" element={<Balance />} />
+          <Route path="/movimiento" element={<Movimiento />} />
+          <Route path="/movimiento" element={<EnvioDinero />} />
+          <Route path="*" component={<NotFound/>}/>
+        </Switch>
       </Routes>
     </div>
   );
