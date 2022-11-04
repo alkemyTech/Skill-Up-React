959ede84635df9b4a4068015db1b12d77b0c9b1b
Lucas Sansberro <lucas_sansberro@outlook.com>
2022-11-04 20:32:13 -0300

    Margin added

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -6,7 +6,7 @@ const NotFound = () => {
 	return (
 		<div className="flex flex-col items-center gap-2 w-full mt-14">
 			<img  src="https://advermedia.ua/wp-content/uploads/2018/04/content5_3.png" alt="404" />
-			<Link to="/" ><Button variant="primary">Back to homepage</Button></Link>
+			<Link to="/" className='mt-10' ><Button variant="primary">Back to homepage</Button></Link>
 		</div>
 	);
 };
