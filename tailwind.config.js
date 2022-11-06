e919adc214d573b366f0f1d185d5f5a5b068e184
Lucas De Palma <lucasdepalma2002@gmail.com>
2022-11-06 20:48:29 -0300

    Variables colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content:["./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-color":"var(--primary-color)",
+        "secondary-color":"var(--secondary-color)",
+        "light-primary-color":"var(--light-primary-color)",
+        "light-sky-blue":"var(--light-sky-blue)",
+        "background-color":"var(--background-color)",
+      }
+    },
   },
   plugins: [],
 }
