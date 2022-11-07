efdd090a5e3b8034135428e6006b1eaa5cb9f52b
Angel Landkoer <angel.landkoer09@gmail.com>
2022-11-07 13:48:01 -0500

    Creando contenido de la pagina de login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,44 @@
-import React from "react";
-import logo from "../assets/img/favi.jfif";
+import { Link } from "react-router-dom";
+// import logo from "../assets/img/favi.jfif";
+
 
 function Login() {
-	return (
-		<header className="App-header">
-			<img src={logo} className="App-logo" alt="logo" />
-			<p className="text-black">Alkemy</p>
-		</header>
-	);
+  return (
+    <>
+      <main>
+        <section className="main flex flex-col justify-center items-center w-full h-auto py-10 bg-stone-200 text-violet-900">
+          <section className="py-12">
+            <h2 className="mb-4 text-4xl font-bold">
+              Bienvenido a Alkemy Bank
+            </h2>
+            <h4 className="mt-4 text-2xl font-semibold">Inicia sesión</h4>
+          </section>
+          <form className="flex flex-col items-center">
+            <label className="flex flex-col items-center">
+              <input
+                className="w-96 p-2 mb-5 border-2 border-solid rounded-lg border-violet-900 placeholder:pl-3 placeholder:text-violet-900"
+                type="email"
+                placeholder="Ingresa tu email"
+                name="email"
+              />
+              <input
+                className="w-96 p-2 mt-3 border-2 border-solid rounded-lg border-violet-900 placeholder:pl-3 placeholder:text-violet-900"
+                type="password"
+                placeholder="Ingresa tu contraseña"
+                name="password"
+              />
+            </label>
+            <button className="w-40 h-11 mt-10 p-2 rounded-lg text-stone-200 bg-violet-900">
+              Inicia sesión
+            </button>
+          </form>
+          <span className="underline font-medium">
+            ¿Aún no tenés cuenta? Registrate acá
+          </span>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default Login;
