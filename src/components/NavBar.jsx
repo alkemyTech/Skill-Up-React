9c0f0be550392b9222422d8d9133d191222311b8
AllegriM <elmonky99@gmail.com>
2022-11-08 20:41:10 -0300

    Add navbar to fix

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,12 +6,12 @@ const NavBar = () => {
 	return (
 		<header className="bg-white">
 			<div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-				<Link to="/home" className="flex title-font font-normal items-center text-gray-900 mb-4 md:mb-0">
+				<Link to="/" className="flex title-font font-normal items-center text-gray-900 mb-4 md:mb-0">
 					<img className="w-full h-10 max-w-md" src={logo} alt="404"/>
 					<span className="ml-3 text-xl">AlkemyBank</span>
 				</Link>
 				<nav className="md:ml-auto flex flex-wrap items-center gap-3 justify-center">
-					<Link className="border-b-2 border-transparent m-1 hover:text-cyan-800 transition-colors duration-300 hover:border-cyan-500" to="/home">
+					<Link className="border-b-2 border-transparent m-1 hover:text-cyan-800 transition-colors duration-300 hover:border-cyan-500" to="/">
 						Home
 					</Link>
 					<Link className="border-b-2 border-transparent m-1 hover:text-cyan-800 transition-colors duration-300 hover:border-cyan-500" to="/envio-de-dinero">
