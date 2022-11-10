021d0d088aef6a8f5b8033539626bb1ab3b48228
Leonel Lombardo <leonelombardo1@gmail.com>
2022-11-10 17:23:36 -0300

    backup

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,11 @@
-export const Button = ({ variant, children, disabled, style, ...props }) => {
+export const Button = ({ variant, type = "button", children, disabled, style, ...props }) => {
 	return (
 		<>
 			{variant === 'primary' ? (
 				<button
 					{...props}
-					className={`lg transition-color max-w-full cursor-pointer rounded-md border border-ct-primary-500 bg-ct-primary-500 py-2 px-4 text-[16px] text-white duration-300 hover:bg-opacity-75 disabled:cursor-not-allowed disabled:border-ct-primary-300 disabled:bg-ct-primary-300 ${style}`}
+					type={type}
+					className={`lg transition-color max-w-full cursor-pointer rounded-md border border-ct-primary-500 bg-ct-primary-500 py-1 px-4 text-[14px] text-white duration-300 hover:bg-opacity-75 disabled:cursor-not-allowed disabled:border-ct-primary-300 disabled:bg-ct-primary-300 ${style}`}
 					disabled={disabled}
 				>
 					{children}
@@ -12,7 +13,8 @@ export const Button = ({ variant, children, disabled, style, ...props }) => {
 			) : variant === 'secondary' ? (
 				<button
 					{...props}
-					className={`transition-color max-w-full cursor-pointer rounded-md border border-white bg-transparent py-2 px-4 text-[16px] text-white duration-300 hover:bg-white hover:bg-opacity-5 ${style}`}
+					type={type}
+					className={`transition-color max-w-full cursor-pointer rounded-md border border-white bg-transparent py-1 px-4 text-[14px] text-white duration-300 hover:bg-white hover:bg-opacity-5 ${style}`}
 					disabled={disabled}
 				>
 					{children}
@@ -20,7 +22,8 @@ export const Button = ({ variant, children, disabled, style, ...props }) => {
 			) : variant === 'tertiary' ? (
 				<button
 					{...props}
-					className={`transition-color max-w-full cursor-pointer rounded-md border border-ct-primary-500 bg-transparent py-2 px-4 text-[16px] text-ct-primary-500 duration-300 hover:bg-ct-primary-500 hover:bg-opacity-5 disabled:cursor-not-allowed disabled:border-ct-primary-300 disabled:text-ct-primary-300 ${style}`}
+					type={type}
+					className={`transition-color max-w-full cursor-pointer rounded-md border border-ct-primary-500 bg-transparent py-1 px-4 text-[14px] text-ct-primary-500 duration-300 hover:bg-ct-primary-500 hover:bg-opacity-5 disabled:cursor-not-allowed disabled:border-ct-primary-300 disabled:text-ct-primary-300 ${style}`}
 					disabled={disabled}
 				>
 					{children}
@@ -28,12 +31,14 @@ export const Button = ({ variant, children, disabled, style, ...props }) => {
 			) : variant === 'mini' ? (
 				<button
 					{...props}
+					type={type}
 					className="transition-color max-w-fit cursor-default rounded-md border border-white bg-transparent py-2 px-4 text-[12px] text-white duration-300 hover:bg-white hover:bg-opacity-5 lg:cursor-pointer"
 				>
 					{children}
 				</button>
 			) : (
-				<button {...props} />
+				<button {...props} 
+				type="button"/>
 			)}
 		</>
 	);
