605697d70329926c0ed0bb98ae2da1186cfbc468
Tomás Sánchez <90143652+stomas418@users.noreply.github.com>
2022-11-11 17:53:21 -0300

    Update UltimosEnvios.jsx

--- src/components/UltimosEnvios.jsx
+++ src/components/UltimosEnvios.jsx
@@ -17,7 +17,7 @@ const UltimosEnvios = () => {
                 },
                 withCredentials: true
             })).json()
-            setEnvios(transactionList.data.filter(envio => envio.userId == user.id).slice(0, 4))
+            setEnvios(transactionList.data.filter(envio => envio.userId == user.id && envio.type == 'payment' && envio.to_account_id).slice(0, 4))
 
         }
         getLastTransactions()
