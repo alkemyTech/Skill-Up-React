443407e18f2f29a365ed741fbb992a197fa0997c
Philip Perez <jperez.c92@gmail.com>
2022-11-11 00:32:12 -0500

    feat: add movements page skeleton

--- src/pages/TransactionsPage/Transactions.page.jsx
+++ src/pages/TransactionsPage/Transactions.page.jsx
@@ -7,9 +7,33 @@ import { MovementCard } from 'src/components/MovementCard';
 import { Pagination } from 'src/components/Pagination';
 import { Select } from 'src/components/Select';
 import { Input } from 'src/components/SignInForm/Input';
+import { Skeleton } from 'src/components/Skeleton';
 import { Text } from 'src/components/Text';
 import { MovementType } from 'src/models/movementType.model';
+import { range } from 'src/utils/range';
 import { useMovementsFilters } from './useMovementsFilters';
+
+const TransactionsSkeleton = () => {
+	return (
+		<div className="mx-auto w-full max-w-screen-xl px-4 py-10 xl:px-0">
+			<Skeleton className="mb-10 h-[40px] rounded md:h-[69px]" />
+			<Skeleton className="h-[301px] w-full rounded md:h-[108px]" />
+
+			<div className="mt-10 grid gap-2 sm:grid-cols-[auto_1fr] md:mt-8">
+				<Skeleton className="ml-auto mb-2 h-[22px] w-[130.56px] rounded" />
+				<Skeleton className="ml-auto h-[40px] w-full rounded sm:max-w-md md:h-[50px]" />
+			</div>
+
+			<div className="my-10 grid grid-cols-[repeat(auto-fill,minmax(min(100%,20rem),1fr))] gap-4">
+				{range(10).map((v) => (
+					<Skeleton key={v} className="h-[111px] w-full rounded md:h-[124px]" />
+				))}
+			</div>
+
+			<Skeleton className="ml-auto h-[40px] w-full rounded sm:max-w-md md:h-[50px]" />
+		</div>
+	);
+};
 
 export default function TransactionsPage() {
 	const movementList = useSelector((state) => state.movements.movementList);
@@ -31,12 +55,12 @@ export default function TransactionsPage() {
 	const [showConcept, setShowConcept] = React.useState(false);
 
 	if (!isLoadedMovementsInfo) {
-		return <main>skeleton</main>;
+		return <TransactionsSkeleton />;
 	}
 
 	return (
-		<main className="mx-auto w-full max-w-screen-xl px-4 py-8 xl:px-0">
-			<Heading className="mb-8 text-ct-neutral-dark-700">Movements</Heading>
+		<main className="mx-auto w-full max-w-screen-xl px-4 py-10 xl:px-0">
+			<Heading className="mb-10 text-ct-neutral-dark-700">Movements</Heading>
 
 			<fieldset className="grid gap-4 rounded border border-ct-neutral-dark-100 px-4 pb-4 shadow-md md:grid-cols-[repeat(4,1fr)]">
 				<Text as="legend" className="px-2 font-bold tracking-wider">
@@ -90,7 +114,7 @@ export default function TransactionsPage() {
 				</Button>
 			</fieldset>
 
-			<div className="mx-auto mt-8 grid gap-2 sm:mr-0 sm:grid-cols-[auto_1fr] ">
+			<div className="mt-8 grid gap-2 sm:grid-cols-[auto_1fr]">
 				<div className="my-2 ml-auto flex items-center sm:ml-0">
 					<Input
 						label="Show concepts"
