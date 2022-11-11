4b024f2f9905efd0ed1d4b6f67cead64eb04ba81
Angel Landkoer <angel.landkoer09@gmail.com>
2022-11-11 16:37:43 -0500

    Eliminando componente y custom hook no utilizados y cambiando contenido del README

--- src/hooks/useModal.jsx
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-
-export function useModal() {
-  const [showModal, setShowModal] = useState(false);
-  const [surePay, setSurePay] = useState(false);
-
-  return { setShowModal, setSurePay, showModal, surePay };
-}
