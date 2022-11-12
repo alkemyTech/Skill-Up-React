cd0d1645f14c2780e0fdd2fc1ca6efd7353dc527
Philip Perez <jperez.c92@gmail.com>
2022-11-12 02:53:19 -0500

    feat: added alert when click quick charge

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -17,27 +17,31 @@ const TitleSize = /**@type {const} */ ({
  *@param {React.ReactNode} [props.children]
  *@param {string} [props.className]
  */
-export const Heading = ({ as: Tag = 'h1', size = TitleSize.headline1, className = '', children, ...props }) => {
+export const Heading = React.forwardRef(function Heading(
+	{ as: Tag = 'h1', size = TitleSize.headline1, className = '', children, ...props },
+	ref,
+) {
 	return (
 		<Tag
 			{...props}
+			ref={ref}
 			className={`${className} ${
 				size === TitleSize.headline1
-					? 'text-5xl font-black uppercase leading-[80%] md:text-7xl'
+					? 'text-5xl font-extrabold uppercase leading-[80%] md:text-7xl'
 					: size === TitleSize.headline2
-					? 'text-4xl font-black uppercase leading-[80%] md:text-5xl'
+					? 'text-4xl font-extrabold uppercase leading-[80%] md:text-5xl'
 					: size === TitleSize.headline3
-					? 'text-3xl font-black uppercase leading-[80%] md:text-4xl'
+					? 'text-3xl font-extrabold uppercase leading-[80%] md:text-4xl'
 					: size === TitleSize.headline4
-					? 'text-2xl font-black uppercase leading-[80%] md:text-3xl'
+					? 'text-2xl font-extrabold uppercase leading-[80%] md:text-3xl'
 					: size === TitleSize.headline5
-					? 'text-xl font-black uppercase leading-[80%] md:text-2xl'
+					? 'text-xl font-extrabold uppercase leading-[80%] md:text-2xl'
 					: size === TitleSize.headline6
-					? 'text-lg font-black uppercase leading-[80%] md:text-xl'
+					? 'text-lg font-extrabold uppercase leading-[80%] md:text-xl'
 					: ''
 			} font-barlow tracking-wide decoration-[none]`}
 		>
 			{children}
 		</Tag>
 	);
-};
+});
--- src/pages/DepositPage/DepositV2.page.jsx
+++ src/pages/DepositPage/DepositV2.page.jsx
@@ -1,12 +1,15 @@
 import { useMutation } from '@tanstack/react-query';
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 import { queryClient } from 'src';
 import { MovementFormToCreate } from 'src/adapters/MovementFormToCreate.adapter';
+import { Alert } from 'src/components/Alert';
 import { Button } from 'src/components/Button';
 import { Heading } from 'src/components/Heading';
 import { Select } from 'src/components/Select';
 import { Input } from 'src/components/SignInForm/Input';
+import { Text } from 'src/components/Text';
 import { currencyCodeDefault } from 'src/models/currencyCodeDefault';
 import { currencyList } from 'src/models/currencyList';
 import { MovementType } from 'src/models/movementType.model';
@@ -39,6 +42,24 @@ const DepositV2 = () => {
 			},
 		},
 	);
+	const { mutate: onQuickCharge } = useMutation(
+		async (amount) => {
+			const newTopUp = MovementFormToCreate({
+				...formValues,
+				amount: amount,
+				concept: 'Quick charge',
+				currencyCode: currencyCodeDefault,
+			});
+
+			const result = await AccounstRepository().movementCreate({ accountId: user.accountId, movementCreate: newTopUp });
+			return result;
+		},
+		{
+			onSuccess: () => {
+				queryClient.invalidateQueries({ queryKey: transactionsQueryKeys.transactions });
+			},
+		},
+	);
 
 	const onChange = (e) => {
 		const { name, value } = e.target;
@@ -49,38 +70,97 @@ const DepositV2 = () => {
 		<main className="mx-auto w-full max-w-screen-xl px-4 py-10 xl:px-0">
 			<Heading className="mb-10 text-ct-neutral-dark-700">Deposits</Heading>
 
-			<form onSubmit={onSubmit} className="mx-auto flex max-w-sm flex-col gap-4">
-				<Select
-					label="Select a currency"
-					onChange={onChange}
-					name={fieldNames.currencyCode}
-					value={formValues.currencyCode}
-				>
-					{currencyList.map((c) => (
-						<option key={c} value={c}>
-							{c}
-						</option>
-					))}
-				</Select>
+			<div className="grid gap-5 md:grid-cols-[1fr_auto_1fr]">
+				<div className="grid grid-rows-[auto_1fr]">
+					<fieldset className="grid grid-cols-[repeat(auto-fill,minmax(6rem,1fr))] gap-4 rounded border border-ct-secondary-400 p-4 shadow-md md:max-w-lg">
+						<Text as="legend" className=" px-4">
+							Quick charge
+						</Text>
+						{[100, 200, 300, 400, 500, 1000, 1500, 2000].map((amount) => (
+							<Alert
+								key={amount}
+								trigger={
+									<Button colorScheme="tertiary" className="whitespace-nowrap">
+										{amount} {currencyCodeDefault}
+									</Button>
+								}
+								title={
+									<Heading size="headline2" className="text-ct-special1-900">
+										Are you sure?
+									</Heading>
+								}
+								description={
+									<Text>
+										You want to charge {currencyCodeDefault} {amount}
+									</Text>
+								}
+								cancelButton={
+									<Button colorScheme="danger" variant="ghost">
+										Cancel
+									</Button>
+								}
+								confirmButton={
+									<Button colorScheme="success" variant="ghost" onClick={() => onQuickCharge(amount)}>
+										Accept
+									</Button>
+								}
+							/>
+						))}
+					</fieldset>
 
-				<Input
-					label="Amount"
-					type="number"
-					max="5000"
-					onChange={onChange}
-					name={fieldNames.amount}
-					value={formValues.amount}
-				/>
-
-				<div className="grid">
-					<label htmlFor="concept">Concept</label>
-					<textarea onChange={onChange} className="border" name="concept" id="concept" cols="30" rows="4"></textarea>
+					<img
+						src="./topup.webp"
+						alt="saving money"
+						className="mt-8 hidden max-w-xs place-self-center md:block xl:max-w-md"
+					/>
 				</div>
 
-				<Button className="" type="submit">
-					Charge money
-				</Button>
-			</form>
+				<hr className="h-full justify-center border-l border-ct-special1-700/30 md:w-[1px]" />
+
+				<form
+					onSubmit={onSubmit}
+					className="mx-auto flex w-full max-w-sm flex-col gap-4 rounded border border-ct-secondary-500/50 p-6 md:h-min"
+				>
+					<Heading as="h2" size="headline3" className="mb-6 text-center text-ct-neutral-dark-700">
+						Custom charge
+					</Heading>
+
+					<Select
+						colorScheme="secondary"
+						label="Select a currency"
+						onChange={onChange}
+						name={fieldNames.currencyCode}
+						value={formValues.currencyCode}
+					>
+						{currencyList.map((c) => (
+							<option key={c} value={c}>
+								{c}
+							</option>
+						))}
+					</Select>
+
+					<Input
+						colorScheme="secondary"
+						label="Amount"
+						type="number"
+						max="5000"
+						onChange={onChange}
+						name={fieldNames.amount}
+						value={formValues.amount}
+					/>
+
+					<div className="grid">
+						<Text as="label" htmlFor="concept" className="font-medium text-ct-neutral-medium-700">
+							Concept
+						</Text>
+						<textarea onChange={onChange} className="border" name="concept" id="concept" cols="30" rows="4"></textarea>
+					</div>
+
+					<Button className="" type="submit">
+						Charge money
+					</Button>
+				</form>
+			</div>
 		</main>
 	);
 };
